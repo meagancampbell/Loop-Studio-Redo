34d4a43174ddda71b894496343e30f80f122b2ab
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-20 19:29:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const input = require('readline-sync');
+
+//Part A #1: Initialize variables to store the following arrays. Remember to use descriptive names.
+let astroNum = 0;
+
+while (astroNum > 6 || astroNum < 0) {
+  mealCount = input.question("How many meals should I prepare?")
+}
+let protein = ['chicken', 'pork', 'tofu', 'beef', 'fish', 'beans'];
+let grain = ['rice', 'pasta', 'corn', 'potato', 'quinoa', 'crackers'];
+let vegetable = ['peas', 'green beans', 'kale','edamame', 'broccoli', 'asparagus'];
+let beverage = ['juice', 'milk', 'water', 'soy milk', 'soda', 'tea'];
+let dessert = ['apple', 'banana', 'more kale','ice cream', 'chocolate', 'kiwi'];
